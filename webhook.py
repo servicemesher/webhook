34c22014401c8d619ac1b17fc691e392b620a419
Vincent Cui <github@fleeto.us>
2018-09-02 18:24:09 +0800

    add logs

--- webhook.py
+++ webhook.py
@@ -8,11 +8,31 @@ import github
 import os
 import re
 
+import logging
+import logging.handlers
 
+LOG_FILE = 'webhook.log'
+MAX_LOG_BYTES = 1024 * 1024
 MAX_WORKING = 3
-TOKEN = os.environ["GITHUB_TOKEN"]
+LOG_LEVEL = os.getenv('LOG_LEVEL', logging.INFO)
+TOKEN = os.getenv('GITHUB_TOKEN', "")
 CMD_LIST = ["/accept", "/pushed", "/merged"]
 ADMIN_LIST = "@fleeto, @rootsongjc"
+
+handler = logging.handlers.RotatingFileHandler(
+    LOG_FILE, maxBytes=MAX_LOG_BYTES, backupCount=50)
+fmt = '%(asctime)s - [%(levelname)s] - %(filename)s:%(lineno)s - %(message)s'
+
+formatter = logging.Formatter(fmt)
+handler.setFormatter(formatter)
+
+logger = logging.getLogger()
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+app = Flask(__name__)
+webhook = Webhook(app)
+
 
 def get_cmd(body):
     cmd = body.strip().lower()
@@ -20,6 +40,7 @@ def get_cmd(body):
         return cmd[1:]
     else:
         return False
+
 
 def get_labels(issue):
     label_list = issue.get_labels()
@@ -47,14 +68,20 @@ def get_issue_by_assignee(repo, login, flag):
     return count < MAX_WORKING
 
 
-app = Flask(__name__)
-webhook = Webhook(app)
-
+def log_incoming_comment(data):
+    line = "User: {} Issue: {} Comment: {}"
+    content = line.format(
+        data["repository"]["owner"]["login"],
+        data["issue"]["number"],
+        data["comment"]["body"]
+    )
+    logger.info(line)
 
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
 
 @webhook.hook("issues")
 def on_issues(data):
@@ -66,12 +93,14 @@ def on_issues(data):
     issue_obj = repo_obj.get_issue(data["issue"]["number"])
     issue_obj.add_to_labels("welcome")
 
+
 @webhook.hook("issue_comment")
 def on_issue_comment(data):
+    log_incoming_comment(data)
     # 检查命令
     cmd = get_cmd(data["comment"]["body"])
     if not cmd:
-        print("Not my style.")
+        logging.info("No availible command found in the issue")
         return
     client = github.Github(TOKEN)
     owner_login = data["repository"]["owner"]["login"]
@@ -79,19 +108,26 @@ def on_issue_comment(data):
     creater_obj = client.get_user(data["sender"]["login"])
     issue_obj = repo_obj.get_issue(data["issue"]["number"])
     if not repo_obj.has_in_assignees(creater_obj):
-        print("Not in member list")
+        logging.info("The author isn't in the list of assignees")
         return
     label_name_list = get_labels(issue_obj)
     assignee_login_list = get_assignees(issue_obj)
+    logging.info(
+        "Current assignees: {}".format(",".join(assignee_login_list))
+    )
+    logging.info(
+        "Current Labels: {}".format(",".join(label_name_list))
+    )
     assigned = len(issue_obj.assignees) > 0
     if (cmd == "accept"):
         if (assigned) or (not ("pending" in label_name_list)):
-            print("The issue had been assigned.")
+            logging.info("The issue had been assigned.")
             return
         if not get_issue_by_assignee(repo_obj, data["sender"]["login"], "translating"):
-            body = "@{}: There are too many issues in your queue. {}".format(data["sender"]["login"], ADMIN_LIST)
+            body = "@{}: There are too many issues in your queue. {}".format(
+                data["sender"]["login"], ADMIN_LIST)
             issue_obj.create_comment(body)
-            return            
+            return
         issue_obj.add_to_assignees(creater_obj)
         issue_obj.remove_from_labels("pending")
         issue_obj.add_to_labels("translating")
@@ -99,24 +135,24 @@ def on_issue_comment(data):
 
     if (cmd == "pushed"):
         if (not ("translating") in label_name_list) or not assigned:
-            print("Can't be pushed.")
+            logging.info("Can't be pushed.")
             return
         if (not data["sender"]["login"] in assignee_login_list):
-            print("You are not member of the assignees")
+            logging.info("You are not member of the assignees")
             return
         issue_obj.remove_from_labels("translating")
         issue_obj.add_to_labels("pushed")
 
     if (cmd == "merged"):
-        if (not ("pushed") in label_name_list) or not assigned:
-            print("Can't be merged.")
+        if (not ("pushed" in label_name_list)) or not assigned:
+            logging.info("Can't be merged.")
             return
         if (not data["sender"]["login"] in assignee_login_list):
-            print("You are not member of the assignees")
+            logging.info("You are not member of the assignees")
             return
         issue_obj.remove_from_labels("pushed")
         issue_obj.add_to_labels("finished")
-        issue_obj.edit(state="closed")        
+        issue_obj.edit(state="closed")
 
 
 if __name__ == "__main__":
